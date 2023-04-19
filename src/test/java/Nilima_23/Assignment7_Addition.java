180704b29ece1d886d847f1a567fef0b9388f384
nilima7864 <nilimaghawate@gmail.com>
2023-04-19 14:40:37 +0530

    Nilima :Assignment 7 method for sum of 2 integers

---
+++ src/test/java/Nilima_23/Assignment7_Addition.java
@@ -0,0 +1,23 @@
+package Nilima_23;
+import java.util.Scanner;
+public class Assignment7_Addition {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter first integer: ");
+		int a = sc.nextInt();
+		System.out.println("Enter second integer: ");
+		int b = sc.nextInt();
+
+		Assignment7_Addition s = new Assignment7_Addition();
+		s.addition(a, b);
+	}
+
+	void addition(int x, int y) {
+		int sum = x + y;
+		System.out.println("Addition of two integer: " + sum);
+
+	}
+
+}
