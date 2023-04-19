fc04625c5c863200468f29c1fe670a860da54491
bhaktibk <bskakade@gmail.com>
2023-04-19 16:29:52 +0530

    removed error

--- src/test/java/Rupesh_23/Reversestring_byword.java
+++ src/test/java/Rupesh_23/Reversestring_byword.java
@@ -1,22 +1,21 @@
 package Rupesh_23;
 
 public class Reversestring_byword {
-	public static String reverseWord(String str){  
-	    String words[]=str.split("\\s");  
-	    String reverseWord="";  
-	    for(String w:words){  
-	        StringBuilder sb=new StringBuilder(w);  
-	        sb.reverse();  
-	        reverseWord+=sb.toString()+" ";  
-	    }  
-	    return reverseWord.trim();  
-	}  
-	
 
-public static class TestStringFormatter {  
-public static void main(String[] args) {  
-    System.out.println(Assignment_3.reverseWord("Today is best day"));  
-       
-    }  
-}
+
+		public static void main(String[] args) {
+			
+			System.out.println(reverseWord("Today is best day"));
+		}
+		
+		public static String reverseWord(String str) {
+			String words[] = str.split("\\s");
+			String reverseWord = "";
+			for (String w : words) {
+				StringBuilder sb = new StringBuilder(w);
+				sb.reverse();
+				reverseWord += sb.toString() + " ";
+			}
+			return reverseWord.trim();
+		}
 }
