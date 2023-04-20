87053bb4df1cf38e3e134f1c5035a0734e0ab8d4
nilima7864 <nilimaghawate@gmail.com>
2023-04-20 18:23:17 +0530

    Nilima Assignment 8 Anagram String

--- src/test/java/Nilima_23/Assignmnet9_AnagramString.java
+++ src/test/java/Nilima_23/Assignmnet9_AnagramString.java
@@ -7,13 +7,16 @@ public class Assignmnet9_AnagramString {
 	public static void main(String[] args) {
 		Assignmnet9_AnagramString obj = new Assignmnet9_AnagramString();
 		obj.isStringAnagram("peek", "seek");
-		obj.isStringAnagram("peek", "keep");
+		obj.isStringAnagram("PEEK", "keep");
 	}
-	
-	
- void isStringAnagram(String str1, String str2){
-	   char[] ch = str1.toCharArray();		
+
+	void isStringAnagram(String str1, String str2) {
+		str1 = str1.replaceAll("\\s", "").toLowerCase();
+		str2 = str2.replaceAll("\\s", "").toLowerCase();
+		
+		char[] ch = str1.toCharArray();
 		Arrays.sort(ch);
+		
 
 		char[] ch1 = str2.toCharArray();
 		Arrays.sort(ch1);
@@ -22,15 +25,11 @@ public class Assignmnet9_AnagramString {
 		System.out.println(result);
 
 		if (result == true) {
-			System.out.println("String is anagram : " +str1 + " = " + str2 );
+			System.out.println("String is anagram : " + str1 + " = " + str2);
 		} else {
-			System.out.println("String is not anagram : " +str1 + " = " + str2);
+			System.out.println("String is not anagram : " + str1 + " = " + str2);
 		}
 
-	 
- }
+	}
 
-
-
-	
 }
