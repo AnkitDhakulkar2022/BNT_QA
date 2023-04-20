1c85469e106d750124c094483ad522e2b217e462
ruppote333 <ruppote333@gmail.com>
2023-04-20 10:12:58 +0530

    remove_spaces_from_string

---
+++ src/test/java/Rupesh_23/remove_spaces_from_string.java
@@ -0,0 +1,13 @@
+package Rupesh_23;
+
+public class remove_spaces_from_string {
+	public static void main(String args[]) {
+	    
+	    String s = " today is best day  ";
+	    System.out.println(s.trim());
+	      
+	    
+	    String s1 = "    today is monday   ";
+	    System.out.println(s1.trim());
+	  }
+}
